415b75ac40924732f29a7d7ef37454ad8b910e25
sintur2527 <sintur2527@gmail.com>
2019-06-10 17:13:31 -0400

    Rendered App basics

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,32 @@
+import VideoListEntry from './VideoListEntry.js';
+
 var VideoList = () => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div>
+      <h5>
+        <em>videoListEntry</em> view goes here
+      </h5>
+    </div>
+    <div>
+      <h5>
+        <em>videoListEntry</em> view goes here
+      </h5>
+    </div>
+    <div>
+      <h5>
+        <em>videoListEntry</em> view goes here
+      </h5>
+    </div>
+    <div>
+      <h5>
+        <em>videoListEntry</em> view goes here
+      </h5>
+    </div>
+    <div>
+      <h5>
+        <em>videoListEntry</em> view goes here
+      </h5>
+    </div>
   </div>
 );
 
--- src/config/youtube.example.js
+++ src/config/youtube.example.js
@@ -1,4 +1,4 @@
 // Put your YouTube API keys here!
-var YOUTUBE_API_KEY = 'YOUR_API_KEY_HERE';
+var YOUTUBE_API_KEY = 'AIzaSyC1QvNuxjnxSuEIjeh8IuGs1uui6gpuoDU';
 
 export default YOUTUBE_API_KEY;
